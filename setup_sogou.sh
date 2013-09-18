9aae693469258be26924be0c742ebe6f31aa3088
Chongguang He <captainhcg@gmail.com>
2013-09-17 20:57:14 -0700

    install pip

--- setup_sogou.sh
+++ setup_sogou.sh
@@ -7,7 +7,7 @@ sudo apt-get remove fcitx
 sudo apt-get autoremove
 sudo add-apt-repository ppa:fcitx-team/nightly
 sudo apt-get update
-sudo apt-get install fcitx fcitx-config-gtk fcitx-sogoupinyin
+sudo apt-get install fcitx fcitx-config-gtk fcitx-sunpinyin fcitx-googlepinyin fcitx-module-cloudpinyin  fcitx-sogoupinyin
 sudo apt-get install fcitx-table-all
 sudo apt-get install im-switch
 im-switch -s fcitx -z default
@@ -17,3 +17,6 @@ sudo apt-get install vim
 
 echo -e "${green}Instaull git\n${endColor}"
 sudo apt-get install git
+
+echo -e "${green}Instaull pip\n${endColor}"
+sudo apt-get install python-pip python-dev build-essential 
